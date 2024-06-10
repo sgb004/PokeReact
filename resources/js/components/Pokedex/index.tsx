827490ad2ca781fa06dd262d3dc0a8229b9fbd38
sgb004 <sgb004@gmail.com>
2024-06-09 20:52:57 -0600

    Added a border to separate the header

--- resources/js/components/Pokedex/index.tsx
+++ resources/js/components/Pokedex/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./style.css";
 
 const Pokedex = () => {
     return (
-        <div className="block w-[100%] h-[90%] max-w-[500px] m-auto bg-pokedex-shadow rounded-[10px]">
+        <div className="pokedex block w-[100%] h-[90%] max-w-[500px] m-auto bg-pokedex-shadow rounded-[10px]">
             <header className="flex p-[20px] gap-[20px]">
                 <div className="camera relative w-[50px] h-[50px] bg-[#fff] rounded-full flex before:content-[''] before:block before:bg-camera before:rounded-full before:m-auto after:content-[''] after:absolute after:block after:bg-camera-light after:w-[18px] after:h-[18px] after:rounded-full after:top-[8px] after:left-[8px]"></div>
                 <div className="indicators flex gap-[10px]">
