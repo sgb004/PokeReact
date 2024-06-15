95ebf01e54c98cbe5f20ec583726adc78594ee93
sgb004 <sgb004@gmail.com>
2024-06-14 19:53:33 -0600

    Completed the front cover

--- resources/js/components/Pokedex/index.tsx
+++ resources/js/components/Pokedex/index.tsx
@@ -3,7 +3,7 @@ import "./style.css";
 
 const Pokedex = () => {
     return (
-        <div className="pokedex block w-[100%] h-[90%] max-w-[500px] m-auto bg-pokedex rounded-[10px] relative">
+        <div className="pokedex block w-[100%] h-[90%] max-w-[500px] m-auto bg-pokedex bg-img-header bg-no-repeat rounded-[10px] relative">
             <header className="flex p-[20px] gap-[20px]">
                 <div className="camera relative w-[50px] h-[50px] bg-[#fff] rounded-full flex before:content-[''] before:block before:bg-camera before:rounded-full before:m-auto after:content-[''] after:absolute after:block after:bg-camera-light after:w-[18px] after:h-[18px] after:rounded-full after:top-[8px] after:left-[8px]"></div>
                 <div className="indicators flex gap-[10px]">
@@ -13,24 +13,41 @@ const Pokedex = () => {
                 </div>
             </header>
 
-            <div className="front-cover absolute w-full h-full top-0 left-0 rounded-bl-[10px] flex items-center flex-wrap bg-pokedex p-[20px] border-r border-r-black after:contents-[''] after:block after:w-[30%] after:h-[15px] after:border after:border-black after:rounded-full after:absolute after:bottom-[20px] after:left-0 after:right-0 after:ml-auto after:mx-auto">
-                <svg
-                    className="triangule cursor-pointer"
-                    width="30"
-                    height="36"
-                    viewBox="0 0 7.937 9.525"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        className="fill-bg-pokedex"
-                        transform="matrix(0 .7723 -.76815 0 7.81 .129)"
-                        stroke="#000"
-                        stroke-width=".333"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M0 10L6 0l6 10z"
-                    />
-                </svg>
+            <div className="front-cover absolute w-full h-full top-0 left-0 rounded-[10px] flex items-center flex-wrap bg-pokedex bg-img-header bg-no-repeat p-[20px] border border-black transition-transform ease-linear duration-1000 after:contents-[''] after:block after:w-[30%] after:h-[15px] after:border after:border-black after:rounded-full after:absolute after:bottom-[20px] after:left-0 after:right-0 after:ml-auto after:mx-auto perspective-[0]">
+                <div className="relative z-20 cursor-pointer">
+                    <svg
+                        className="triangule animate-front-cover-triangule fill-[--bg-color]"
+                        width="30"
+                        height="36"
+                        viewBox="0 0 7.937 9.525"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            className="fill-bg-pokedex"
+                            transform="matrix(0 .7723 -.76815 0 7.81 .129)"
+                            stroke="#000"
+                            stroke-width=".333"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M0 10L6 0l6 10z"
+                        />
+                    </svg>
+                    <label className="front-cover-button-close absolute w-full h-full block top-0 cursor-pointer">
+                        <input
+                            type="radio"
+                            className="front-cover-dispatcher-close hidden"
+                            name="front-cover-button"
+                        />
+                    </label>
+                    <label className="front-cover-button-open absolute w-full h-full block top-0 cursor-pointer">
+                        <input
+                            type="radio"
+                            className="front-cover-dispatcher-open hidden"
+                            name="front-cover-button"
+                        />
+                    </label>
+                </div>
+                <div className="front-cover-reverse invisible absolute w-full bg-pokedex bottom-0 z-10 top-[110px] left-[70px] rounded-[5px] border border-black"></div>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,20 @@ export default {
                 "indicator-green": "#4d905e",
                 "indicator-green-light": "#7ca880",
             },
+            backgroundImage: {
+                "img-header":
+                    'url(\'data:image/svg+xml,<svg width="500" height="91" xmlns="http://www.w3.org/2000/svg"><path d="M0 90h87.5c.833 0 1.757-.158 2.553-.407.795-.248 1.317-.542 2.002-1.016l60.89-42.154s1.363-.84 2.119-1.078c.782-.247 2.436-.345 2.436-.345H500V0" fill="none" stroke="%23000" stroke-linecap="round" stroke-linejoin="round"/></svg>\')',
+            },
+            keyframes: {
+                "front-cover-triangule": {
+                    "0%": { filter: "drop-shadow(0 0 0px var(--bg-color))" },
+                    "100%": { filter: "drop-shadow(0 0 50px var(--bg-color))" },
+                },
+            },
+            animation: {
+                "front-cover-triangule":
+                    "front-cover-triangule 2s infinite ease-in-out",
+            },
         },
     },
     plugins: [],
