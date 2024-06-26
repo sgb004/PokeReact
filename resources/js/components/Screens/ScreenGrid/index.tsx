df9eb7066d811b2c4c990ec7c8fe45bcc6a49a42
sgb004 <sgb004@gmail.com>
2024-06-25 19:53:26 -0600

    Added stop propagation of the wheel

--- resources/js/components/Screens/ScreenGrid/index.tsx
+++ resources/js/components/Screens/ScreenGrid/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PokemonPokedex } from "../../../types";
 
 type ScreenGridProps = {
@@ -7,6 +7,7 @@ type ScreenGridProps = {
 };
 
 const ScreenGrid = ({ getUrl, noPokemonMessage }: ScreenGridProps) => {
+    const screenGridRef = useRef<HTMLDivElement>(null);
     const [pokemon, setPokemon] = useState<PokemonPokedex[]>([]);
 
     useEffect(() => {
@@ -18,8 +19,26 @@ const ScreenGrid = ({ getUrl, noPokemonMessage }: ScreenGridProps) => {
             });
     }, [getUrl]);
 
+    useEffect(() => {
+        const screenGrid = screenGridRef.current;
+        const handleWheel = (event: WheelEvent) => event.stopPropagation();
+
+        if (screenGrid) {
+            screenGrid.addEventListener("wheel", handleWheel);
+        }
+
+        return () => {
+            if (screenGrid) {
+                screenGrid.removeEventListener("wheel", handleWheel);
+            }
+        };
+    }, []);
+
     return (
-        <section className="grid grid-cols-3 overflow-y-auto overflow-x-hidden p-[5px] gap-[5px] bg-screen-grid">
+        <section
+            ref={screenGridRef}
+            className="grid grid-cols-3 overflow-y-auto overflow-x-hidden p-[5px] gap-[5px] bg-screen-grid"
+        >
             {pokemon.length == 0 ? (
                 <div className="no-pokemon col-start-1 col-end-4 flex justify-center items-center">
                     {noPokemonMessage}
