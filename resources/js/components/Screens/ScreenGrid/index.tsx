c33341f4444c68678392af6a05c1082e8b51cae6
sgb004 <sgb004@gmail.com>
2024-06-26 19:18:17 -0600

    Added code to load more pokemon before reaching the end of the scroll

--- resources/js/components/Screens/ScreenGrid/index.tsx
+++ resources/js/components/Screens/ScreenGrid/index.tsx
@@ -6,30 +6,72 @@ type ScreenGridProps = {
     noPokemonMessage: string;
 };
 
+const getTime = () => {
+    const date = new Date();
+    const time = date.getTime();
+    return time;
+};
+
 const ScreenGrid = ({ getUrl, noPokemonMessage }: ScreenGridProps) => {
     const screenGridRef = useRef<HTMLDivElement>(null);
-    const [pokemon, setPokemon] = useState<PokemonPokedex[]>([]);
+    const nextPageUrl = useRef(getUrl);
+    const pokemon = useRef<PokemonPokedex[]>([]);
+    const isLoading = useRef(false);
+    const [render, setRender] = useState(1);
 
     useEffect(() => {
-        fetch(getUrl)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setPokemon(data.data);
-            });
-    }, [getUrl]);
+        isLoading.current = false;
+    }, [render]);
 
     useEffect(() => {
         const screenGrid = screenGridRef.current;
         const handleWheel = (event: WheelEvent) => event.stopPropagation();
+        let handleScroll = () => {};
+
+        const loadMore = () => {
+            if (nextPageUrl.current === null) return false;
+
+            fetch(nextPageUrl.current)
+                .then((response) => response.json())
+                .then((data) => {
+                    nextPageUrl.current = data.next_page_url;
+
+                    for (const item of data.data) {
+                        pokemon.current.push({
+                            id: item.id,
+                            name: item.name,
+                        });
+                    }
+
+                    setRender(getTime());
+                });
+        };
+
+        loadMore();
 
         if (screenGrid) {
+            handleScroll = () => {
+                const scrollTop = screenGrid.scrollTop;
+                const scrollHeight = screenGrid.scrollHeight;
+                const offsetHeight = screenGrid.offsetHeight;
+
+                const scrollPercent =
+                    scrollTop / Math.abs(scrollHeight - offsetHeight);
+                const scrollPercentRounded = Math.ceil(scrollPercent * 100);
+
+                if (!isLoading.current && scrollPercentRounded > 75) {
+                    isLoading.current = true;
+                    loadMore();
+                }
+            };
             screenGrid.addEventListener("wheel", handleWheel);
+            screenGrid.addEventListener("scroll", handleScroll);
         }
 
         return () => {
             if (screenGrid) {
                 screenGrid.removeEventListener("wheel", handleWheel);
+                screenGrid.removeEventListener("scroll", handleScroll);
             }
         };
     }, []);
@@ -39,25 +81,34 @@ const ScreenGrid = ({ getUrl, noPokemonMessage }: ScreenGridProps) => {
             ref={screenGridRef}
             className="grid grid-cols-3 overflow-y-auto overflow-x-hidden p-[5px] gap-[5px] bg-screen-grid"
         >
-            {pokemon.length == 0 ? (
+            {pokemon.current.length == 0 ? (
                 <div className="no-pokemon col-start-1 col-end-4 flex justify-center items-center">
                     {noPokemonMessage}
                 </div>
             ) : (
-                pokemon.map((pokemon) => (
-                    <div
-                        key={pokemon.id}
-                        className="pokemon flex flex-col items-center"
-                    >
-                        <img
-                            className="w-[80%]"
-                            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
-                        />
-                        <div className="name text-black first-letter:uppercase">
-                            {pokemon.name}
+                <>
+                    {pokemon.current.map((pokemon) => (
+                        <div
+                            key={pokemon.id}
+                            className="pokemon flex flex-col items-center"
+                        >
+                            <img
+                                className="w-[80%] aspect-[1/1]"
+                                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+                            />
+                            <div className="name text-black first-letter:uppercase">
+                                {pokemon.name}
+                            </div>
                         </div>
-                    </div>
-                ))
+                    ))}
+                    {nextPageUrl.current ? (
+                        <div className="spinner col-start-1 col-end-4 flex justify-center items-center">
+                            Loading...
+                        </div>
+                    ) : (
+                        <></>
+                    )}
+                </>
             )}
         </section>
     );
