2132555bbc8b777d370c36e71f46849b58578eda
sgb004 <sgb004@gmail.com>
2024-06-27 13:45:24 -0600

    Changed to use params

--- app/Http/Controllers/Api/PokedexController.php
+++ app/Http/Controllers/Api/PokedexController.php
@@ -8,8 +8,27 @@ use Illuminate\Http\Request;
 
 class PokedexController extends Controller
 {
-    public function index(){
-		$data = Pokedex::paginate(30);
+    public function index(Request $request){
+		$filters = ['number' => 'api_id', 'name' => 'name'];
+		$filter = $request->get('filter', 'number');
+		$filter = $filters[$filter] ?? 'api_id';
+
+		$sorts = ['asc' => 'asc', 'desc' => 'desc'];
+		$sort = $request->get('sort', 'asc');
+		$sort = $sorts[$sort] ?? 'asc';
+
+		$search = trim($request->get('search', ''));
+
+		$data = Pokedex::when($search, function ($query) use ($search) {
+			return $query->where('name', 'like', $search . '%');
+		})
+		->orderBy($filter, $sort)
+		->paginate(30)
+		->appends([
+			'filter' => $filter == 'api_id' ? 'number' : 'name',
+			'sort' => $sort,
+			'search' => $search,
+		]);
 
 		return response()->json($data, 200);
 	}
