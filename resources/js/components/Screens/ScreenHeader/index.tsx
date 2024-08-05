4e1d13075cde278a15d14d71f97d290219fdf028
sgb004 <sgb004@gmail.com>
2024-08-05 15:23:14 -0600

    Added imperative methods to set and get the params Changed useState and useEffect to use useRef instead the current value of the params, a method was added to invoke onChange when params change

--- resources/js/components/Screens/ScreenHeader/index.tsx
+++ resources/js/components/Screens/ScreenHeader/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useRef, useImperativeHandle, forwardRef } from "react";
 import { ScreenFilters, ScreenHeaderParams } from "../../../types";
 
 export type ScreenHeader = {
@@ -7,102 +7,137 @@ export type ScreenHeader = {
     onChange: (params: ScreenHeaderParams) => void;
 };
 
-const ScreenHeader = ({ headerParams, filters, onChange }: ScreenHeader) => {
-    const searchTimer = useRef<ReturnType<typeof setTimeout> | null>(null);
+export type ScreenHeaderElement = {
+    current: HTMLElement | null;
+    setParams: (params: ScreenHeaderParams) => void;
+    getParams: () => ScreenHeaderParams;
+} & HTMLElement;
 
-    const [params, setParams] = useState<ScreenHeaderParams>(headerParams);
+const ScreenHeader = forwardRef<HTMLElement, ScreenHeader>(
+    ({ headerParams, filters, onChange }, ref) => {
+        const screenHeaderRef = useRef(null);
 
-    useEffect(() => {
-        onChange(params);
-    }, [params]);
+        const searchTimer = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-    return (
-        <header className="screen-header flex gap-[5px] p-[5px] bg-header">
-            {!filters ? null : (
-                <>
-                    <label className="relative block screen-header-button">
-                        <svg
-                            className="absolute screen-header-button-icon m-auto top-0 bottom-0 left-0 right-0"
-                            fill="none"
-                            height="30"
-                            strokeWidth="1.5"
-                            width="30"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 30 30"
-                        >
-                            <path
-                                d="M2.7 1.162h24.6c.85 0 1.537.7 1.537 1.562v2.477c0 .414-.161.812-.45 1.105l-9.862 10.019c-.288.293-.45.69-.45 1.104v9.849c0 1.016-.94 1.761-1.91 1.515l-3.076-.781a1.558 1.558 0 01-1.164-1.515v-9.068c0-.414-.162-.811-.45-1.104L1.612 6.305a1.575 1.575 0 01-.45-1.104V2.724A1.55 1.55 0 012.7 1.162z"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2.325"
-                            />
-                        </svg>
-                        <select
-                            className="opacity-0 relative size-screen-header-button cursor-pointer"
-                            onChange={(event) => {
-                                setParams({
-                                    ...params,
-                                    filter: event.target.value,
-                                });
-                            }}
-                        >
-                            {filters.map((filter) => (
-                                <option key={filter.name} value={filter.value}>
-                                    {filter.name}
-                                </option>
-                            ))}
-                        </select>
-                    </label>
-                    <label className="cursor-pointer screen-header-button filter-button flex justify-center items-center">
-                        <input
-                            type="checkbox"
-                            className="hidden filter-button-dispatcher"
-                            onChange={(event) => {
-                                setParams({
-                                    ...params,
-                                    sort: event.target.checked ? "desc" : "asc",
-                                });
-                            }}
-                        />
-                        <svg
-                            className="screen-header-button-icon"
-                            fill="none"
-                            height="30"
-                            strokeWidth="1.5"
-                            width="30"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 30 30"
-                        >
-                            <g
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
+        const paramsRef = useRef(headerParams);
+
+        const setParams = (params: ScreenHeaderParams) => {
+            paramsRef.current = params;
+            onChange(params);
+        };
+
+        useImperativeHandle(
+            ref,
+            () => {
+                return {
+                    current: screenHeaderRef.current,
+                    setParams,
+                    getParams: () => {
+                        return paramsRef.current;
+                    },
+                } as ScreenHeaderElement;
+            },
+            []
+        );
+
+        return (
+            <header
+                ref={screenHeaderRef}
+                className="screen-header flex gap-[5px] p-[5px] bg-header"
+            >
+                {!filters ? null : (
+                    <>
+                        <label className="relative block screen-header-button">
+                            <svg
+                                className="absolute screen-header-button-icon m-auto top-0 bottom-0 left-0 right-0"
+                                fill="none"
+                                height="30"
+                                strokeWidth="1.5"
+                                width="30"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 30 30"
                             >
                                 <path
-                                    d="M15 28.953a2.79 2.79 0 110-5.58 2.79 2.79 0 010 5.58zM15 19.186V1.046m0 0l4.186 4.187M15 1.047l-4.186 4.186"
-                                    strokeWidth="2.093025"
+                                    d="M2.7 1.162h24.6c.85 0 1.537.7 1.537 1.562v2.477c0 .414-.161.812-.45 1.105l-9.862 10.019c-.288.293-.45.69-.45 1.104v9.849c0 1.016-.94 1.761-1.91 1.515l-3.076-.781a1.558 1.558 0 01-1.164-1.515v-9.068c0-.414-.162-.811-.45-1.104L1.612 6.305a1.575 1.575 0 01-.45-1.104V2.724A1.55 1.55 0 012.7 1.162z"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2.325"
                                 />
-                            </g>
-                        </svg>
-                    </label>
-                </>
-            )}
-            <input
-                type="text"
-                placeholder="Search"
-                className="rounded-[3px] p-[3px_5px] h-[30px] w-[100%]"
-                onChange={(event) => {
-                    if (searchTimer.current) clearTimeout(searchTimer.current);
-                    searchTimer.current = setTimeout(() => {
-                        setParams({
-                            ...params,
-                            search: event.target.value,
-                        });
-                    }, 300);
-                }}
-            />
-            {/*
+                            </svg>
+                            <select
+                                className="opacity-0 relative size-screen-header-button cursor-pointer"
+                                onChange={(event) => {
+                                    setParams({
+                                        ...paramsRef.current,
+                                        filter: event.target.value,
+                                    });
+                                }}
+                                value={paramsRef.current.filter}
+                            >
+                                {filters.map((filter) => (
+                                    <option
+                                        key={filter.name}
+                                        value={filter.value}
+                                    >
+                                        {filter.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </label>
+                        <label className="cursor-pointer screen-header-button filter-button flex justify-center items-center">
+                            <input
+                                type="checkbox"
+                                className="hidden filter-button-dispatcher"
+                                onChange={(event) => {
+                                    setParams({
+                                        ...paramsRef.current,
+                                        sort: event.target.checked
+                                            ? "desc"
+                                            : "asc",
+                                    });
+                                }}
+                                checked={paramsRef.current.sort === "desc"}
+                            />
+                            <svg
+                                className="screen-header-button-icon"
+                                fill="none"
+                                height="30"
+                                strokeWidth="1.5"
+                                width="30"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 30 30"
+                            >
+                                <g
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path
+                                        d="M15 28.953a2.79 2.79 0 110-5.58 2.79 2.79 0 010 5.58zM15 19.186V1.046m0 0l4.186 4.187M15 1.047l-4.186 4.186"
+                                        strokeWidth="2.093025"
+                                    />
+                                </g>
+                            </svg>
+                        </label>
+                    </>
+                )}
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="rounded-[3px] p-[3px_5px] h-[30px] w-[100%]"
+                    onChange={(event) => {
+                        if (searchTimer.current)
+                            clearTimeout(searchTimer.current);
+                        searchTimer.current = setTimeout(() => {
+                            setParams({
+                                ...paramsRef.current,
+                                search: event.target.value,
+                            });
+                        }, 300);
+                    }}
+                />
+                {/*
             <button className="screen-header-button flex justify-center items-center">
                 <svg
                     className="screen-header-button-icon"
@@ -126,8 +161,9 @@ const ScreenHeader = ({ headerParams, filters, onChange }: ScreenHeader) => {
                 </svg>
             </button>
 			 */}
-        </header>
-    );
-};
+            </header>
+        );
+    }
+);
 
 export default ScreenHeader;
