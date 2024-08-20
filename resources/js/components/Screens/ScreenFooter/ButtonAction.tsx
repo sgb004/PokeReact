73a5c682893221485b214e4eeed7450805dbcb61
sgb004 <sgb004@gmail.com>
2024-08-20 15:06:19 -0600

    Changed unified the border of the action buttons

--- resources/js/components/Screens/ScreenFooter/ButtonAction.tsx
+++ resources/js/components/Screens/ScreenFooter/ButtonAction.tsx
@@ -14,7 +14,7 @@ const ButtonAction = ({
     return (
         <button
             key={action.name}
-            className={`screen-button-action ${action.name} flex justify-center items-center align-items-center absolute rounded-full border border-black w-[40px] h-[40px] right-[20px] transition-all z-10 ${className}`}
+            className={`screen-button-action ${action.name} flex justify-center items-center align-items-center absolute rounded-full border w-[40px] h-[40px] right-[20px] transition-all z-10 border-bg-border-gray ${className}`}
             onClick={(event) => action.action(event)}
             style={{ bottom: `${position * 50 + 10}px` }}
         >
