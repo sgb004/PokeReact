dc1d53f4a890aa24f0a7416b8b35a75acbe6d1a9
sgb004 <sgb004@gmail.com>
2024-08-21 15:06:16 -0600

    Added prop classname

--- resources/js/components/PokemonImg/index.tsx
+++ resources/js/components/PokemonImg/index.tsx
@@ -1,11 +1,12 @@
 type PokemonImgProps = {
     number: number;
+    className?: string;
 };
 
-const PokemonImg = ({ number }: PokemonImgProps) => (
+const PokemonImg = ({ number, className = "" }: PokemonImgProps) => (
     <>
         <img
-            className="w-[80%] aspect-[1/1]"
+            className={`w-[80%] aspect-[1/1] ${className}`}
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`}
         />
     </>
