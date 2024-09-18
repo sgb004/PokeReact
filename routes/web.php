6d1f17b52b8f14f3e27118d0d6478e87c8ae8e64
sgb004 <sgb004@gmail.com>
2024-09-17 18:34:21 -0600

    Added route to download the Pokémon

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PokedexController;
+use App\Http\Controllers\MyPokedexController;
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/pokedex/store-pokedex', [PokedexController::class, 'storePokedex']);
+Route::get('/my-pokedex/download', [MyPokedexController::class, 'download']);
 
