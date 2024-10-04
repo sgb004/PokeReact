a11ac638c4407a60b8a1f48d5a940e9447cf8cbf
sgb004 <sgb004@gmail.com>
2024-10-04 14:35:43 -0600

    Change to use useImperativeHandle and add a method to show error messages

--- resources/js/components/Input/index.tsx
+++ resources/js/components/Input/index.tsx
@@ -1,8 +1,23 @@
-import { InputHTMLAttributes, useState } from "react";
+import {
+    forwardRef,
+    InputHTMLAttributes,
+    useImperativeHandle,
+    useRef,
+    useState,
+} from "react";
 import validateValue from "../../validations/validateValue";
 import validateUnsignedInteger from "../../validations/validateUnsignedInteger";
 
-const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
+export type InputElement = {
+    element: HTMLInputElement | null;
+    setErrorMessage: (message: string) => void;
+} & HTMLInputElement;
+
+const Input = forwardRef<
+    HTMLInputElement,
+    InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
+    const inputRef = useRef(null);
     const [error, setError] = useState("");
 
     const handleChange = (value: string) => {
@@ -17,9 +32,23 @@ const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
         setError(error);
     };
 
+    useImperativeHandle(
+        ref,
+        () => {
+            return {
+                element: inputRef.current,
+                setErrorMessage: (message: string) => {
+                    setError(message);
+                },
+            } as InputElement;
+        },
+        []
+    );
+
     return (
         <div>
             <input
+                ref={inputRef}
                 {...props}
                 onChange={(event) => {
                     props.onChange?.(event);
@@ -27,7 +56,7 @@ const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
                 }}
             />
             {error ? (
-                <div className="tooltip absolute left-[15px] mt-[5px] text-capitalize flex justify-center w-[calc(100%-30px)] z-10">
+                <div className="tooltip error-message absolute left-[15px] mt-[5px] text-capitalize flex justify-center w-[calc(100%-30px)] z-10">
                     <div className="bg-red-500 text-white text-center p-2 rounded-md m-auto relative before:[content:''] before:block before:absolute before:top-[-5px] before:left-[0] before:right-[0] before:mx-auto before:w-0 before:h-0 before:border-[transparent_transparent_#ef4444_transparent] before:border-t-[0] before:border-x-[5px] before:border-b-[5px]">
                         {error}
                     </div>
@@ -37,6 +66,6 @@ const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
             )}
         </div>
     );
-};
+});
 
 export default Input;
