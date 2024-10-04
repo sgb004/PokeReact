f7f2bf426cd6cb90ad3d5324384702d870abbc8c
sgb004 <sgb004@gmail.com>
2024-10-04 16:52:04 -0600

    Fixed the vertical scroll

--- resources/js/components/Pokedex/index.tsx
+++ resources/js/components/Pokedex/index.tsx
@@ -1,18 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "./style.css";
 import Navigation from "../Navigation";
 import Screens from "../Screens";
 import FrontCoverReverse from "../FrontCoverReverse";
 
 const Pokedex = () => {
+    const pokedexRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
+        if (!(pokedexRef.current instanceof HTMLDivElement)) return () => {};
+
+        const pokedexElement = pokedexRef.current;
         let touchX = 0;
 
         const moveScroll = (direction: number) => {
             const [left, top] =
                 direction > 0 ? [document.body.scrollWidth, 0] : [0, 0];
 
-            window.scrollTo({ left, top, behavior: "smooth" });
+            pokedexElement.scrollTo({ left, top, behavior: "smooth" });
         };
 
         const handleWheel = (event: WheelEvent) => moveScroll(event.deltaY);
@@ -25,19 +30,22 @@ const Pokedex = () => {
             touchX = event.touches[0].pageX;
         };
 
-        window.addEventListener("wheel", handleWheel);
-        window.addEventListener("touchstart", handleTouchStart);
-        window.addEventListener("touchmove", handleTouchMove);
+        pokedexElement.addEventListener("wheel", handleWheel);
+        pokedexElement.addEventListener("touchstart", handleTouchStart);
+        pokedexElement.addEventListener("touchmove", handleTouchMove);
 
         return () => {
-            window.removeEventListener("wheel", handleWheel);
-            window.removeEventListener("touchstart", handleTouchStart);
-            window.removeEventListener("touchmove", handleTouchMove);
+            pokedexElement.removeEventListener("wheel", handleWheel);
+            pokedexElement.removeEventListener("touchstart", handleTouchStart);
+            pokedexElement.removeEventListener("touchmove", handleTouchMove);
         };
     }, []);
 
     return (
-        <div className="pokedex grid grid-cols-[175px_1fr_20px] grid-rows-[94px_1fr] w-[100%] h-[90%] max-w-[500px] m-auto bg-pokedex bg-img-header bg-no-repeat bg-position rounded-[10px] relative border border-black before:content-[''] before:block before:absolute before-t-0 before:w-full before:h-[95px] before:bg-img-header before:bg-left-bottom before:bg-no-repeat">
+        <div
+            ref={pokedexRef}
+            className="pokedex grid grid-cols-[175px_1fr_20px] grid-rows-[94px_1fr] w-full h-full max-h-[920px] max-w-[500px] m-auto bg-pokedex bg-img-header bg-no-repeat bg-position rounded-[10px] relative border border-black overflow-hidden before:content-[''] before:block before:absolute before-t-0 before:w-full before:h-[95px] before:bg-img-header before:bg-left-bottom before:bg-no-repeat "
+        >
             <header className="flex p-[20px] gap-[20px] rounded-t-[10px] relative">
                 <div className="camera relative w-[50px] h-[50px] bg-[#fff] rounded-full flex before:content-[''] before:block before:bg-camera before:rounded-full before:m-auto after:content-[''] after:absolute after:block after:bg-camera-light after:w-[18px] after:h-[18px] after:rounded-full after:top-[8px] after:left-[8px]"></div>
                 <div className="indicators flex gap-[10px]">
