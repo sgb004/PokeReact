efe972b5fd89ba031f2773da8ef6fbc2c8ae6ec2
sgb004 <sgb004@gmail.com>
2024-10-04 16:03:42 -0600

    Changed to expose the host

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-    plugins: [
-        react(),
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.jsx"],
-            refresh: true,
-        }),
-    ],
-});
+export default ({ mode }) => {
+    let host = "localhost";
+
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    host = process.env.VITE_APP_HOST || host;
+
+    return defineConfig({
+        plugins: [
+            react(),
+            laravel({
+                input: ["resources/css/app.css", "resources/js/app.jsx"],
+                refresh: true,
+            }),
+        ],
+        server: {
+            hmr: {
+                host,
+            },
+            host,
+        },
+    });
+};
