8ce1c168e87bd33ecea8c6aed41e576c1fbe193a
sgb004 <sgb004@gmail.com>
2024-10-04 20:19:23 -0600

    Added code to display a scroll if the dialog content has a height larger than the container

--- resources/js/components/Dialog/index.tsx
+++ resources/js/components/Dialog/index.tsx
@@ -32,7 +32,7 @@ const Dialog = forwardRef<HTMLDialogElement, DialogProps>(
                 <div className="w-full h-full flex items-center justify-center p-[15px]">
                     <form
                         method="dialog"
-                        className="bg-white min-w-[200px] p-[15px] rounded-[5px] animate-in"
+                        className="bg-white max-h-full min-w-[200px] overflow-auto p-[15px] rounded-[5px] animate-in"
                     >
                         <p className="leading-[1.3] mb-[10px]">{message}</p>
                         <div className="dialog-actions flex justify-center gap-[10px]">
