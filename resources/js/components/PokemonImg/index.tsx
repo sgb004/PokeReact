af3263b7fdf0a8f1d08a7bc2a3040c003d438a71
sgb004 <sgb004@gmail.com>
2024-10-07 21:26:36 -0600

    Added transition after to load and alt img if the image not found

--- resources/js/components/PokemonImg/index.tsx
+++ resources/js/components/PokemonImg/index.tsx
@@ -6,8 +6,14 @@ type PokemonImgProps = {
 const PokemonImg = ({ number, className = "" }: PokemonImgProps) => (
     <>
         <img
-            className={`w-[80%] aspect-[1/1] ${className}`}
+            className={`pokemon-img w-[80%] aspect-[1/1] ${className} transition-opacity opacity-0`}
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`}
+            onLoad={(event) => {
+                event.currentTarget.classList.add("opacity-100");
+            }}
+            onError={(event) => {
+                event.currentTarget.src = "/images/question.svg";
+            }}
         />
     </>
 );
