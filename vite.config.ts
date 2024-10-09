093332c453d2f12944e63e28ff328b9f1ada3bcb
sgb004 <sgb004@gmail.com>
2024-10-09 15:43:02 -0600

    Changed to build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
-export default ({ mode }) => {
+export default ({ mode }: { mode: string }) => {
     let host = "localhost";
 
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -12,7 +12,7 @@ export default ({ mode }) => {
         plugins: [
             react(),
             laravel({
-                input: ["resources/css/app.css", "resources/js/app.jsx"],
+                input: ["resources/css/app.css", "resources/js/app.tsx"],
                 refresh: true,
             }),
         ],
