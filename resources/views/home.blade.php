c62a4886a223ce65449a3a3e8991bec061f2bbaf
sgb004 <sgb004@gmail.com>
2024-10-09 16:27:55 -0600

    Changed the title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>PokéReact a Pokédex React</title>
+	<title>PokéReact a Pokédex made in React</title>
 
 	<link rel="icon" href="favicon.svg" type="image/svg+xml">
 
